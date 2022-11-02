418acd3d36b485a26b5838418961d11eb55b94fe
Ruslan Volovenko <vokoloven@gmail.com>
2022-11-02 23:10:52 +0200

    update

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="goit-react-hw-05-movies">
       <ThemeProvider theme={theme}>
         <Suspense>
           <App />
